8bf10b3e05778269a14159db0d4e9a07a133e0a9
fmalmeida <felipemarques89@gmail.com>
2021-05-08 13:35:01 -0300

    update

--- README.md
+++
@@ -1,7 +0,0 @@
-# Olá
-
-TESTE
-
-continuar
-
-[Google Colab](https://colab.research.google.com/drive/1ernIvARLTWU2Wupg-piPCnbIy9IYbHRP?usp=sharing)
--- setup_env.sh
+++
@@ -1,8 +0,0 @@
-# download conda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O instalador_conda.sh
-
-# install conda
-bash instalador_conda.sh -b -p ./miniconda
-
-# install blast
-conda install -y -c bioconda -c conda-forge -c defaults blast
